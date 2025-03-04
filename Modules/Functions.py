8209172fc14922917b176076557505e05cb76aa5
BernTPB03 <bernardobarros8@gmail.com>
2025-03-04 03:11:28 -0300

    functions with using

---
+++ Modules/Functions.py
@@ -0,0 +1,14 @@
+# #here we learn how to define and use Functions
+def multiply(number):
+    result = number * 2
+    return result
+
+print(multiply(10))
+print(multiply(4))
+print(multiply(130))
+
+#-------->Text<-------------
+def printing(text):
+    print("the text is",text)
+
+printing("carlos")
